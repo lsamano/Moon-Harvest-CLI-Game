85bcced1d5b6522a3b6f543046bd64e1ea8685d4
lsamano <leizl.samano@gmail.com>
2019-07-20 13:53:56 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,69 @@
-# require_relative '../config/environment'
-# Farmer.destroy_all
-# Farmer.create(name: "Yeye")
+# Destroy Previous
+Product.destroy_all
+Livestock.destroy_all
+SeedBag.destroy_all
+CropType.destroy_all
+Animal.destroy_all
+Farmer.destroy_all
 
-yeye = Farmer.first
-cow = Animal.create(species: "cow", product_name: "milk", sell_price: 400, frequency: 1)
-sheep = Animal.create(species: "sheep", product_name: "wool", sell_price: 1000, frequency: 3)
+# Make New Default Farmer
+farmer = Farmer.create(name: "Clarabelle")
 
-bessie = Livestock.create(animal_id: cow.id, farmer_id: yeye.id, name: "Bessie", love: 1, brushed: 0, fed: 0, counter: 1)
-wooly = Livestock.create(animal_id: sheep.id, farmer_id: yeye.id, name: "Wooly", love: 1, brushed: 0, fed: 0, counter: 1)
+# Create Animals
+cow = Animal.create(
+  species: "cow",
+  product_name: "milk",
+  sell_price: 400,
+  frequency: 1
+)
+sheep = Animal.create(
+  species: "sheep",
+  product_name: "wool",
+  sell_price: 1000,
+  frequency: 3
+)
 
-Product.create(livestock_id: bessie.id)
-Product.create(livestock_id: wooly.id)
+# Create Livestock for Default Farmer
+bessie = Livestock.create(animal_id: cow.id, farmer_id: farmer.id, name: "Bessie", love: 1, brushed: 0, fed: 0, counter: 1)
+wooly = Livestock.create(animal_id: sheep.id, farmer_id: farmer.id, name: "Wooly", love: 1, brushed: 0, fed: 0, counter: 1)
 
-# ### Fall Crops
-# CropType.create(
-#   crop_name: "carrot",
-#   days_to_grow: 6,
-#   buy_price: 390,
-#   season: "fall",
-#   sell_price: 1710
-# )
-# CropType.create(
-#   crop_name: "sweet potato",
-#   days_to_grow: 6,
-#   buy_price: 240,
-#   season: "fall",
-#   sell_price: 990
-# )
-# CropType.create(
-#   crop_name: "spinach",
-#   days_to_grow: 5,
-#   buy_price: 440,
-#   season: "fall",
-#   sell_price: 1890
-# )
-# CropType.create(
-#   crop_name: "eggplant",
-#   days_to_grow: 11,
-#   buy_price: 480,
-#   season: "fall",
-#   sell_price: 4140
-# )
-# CropType.create(
-#   crop_name: "bell pepper",
-#   days_to_grow: 11,
-#   buy_price: 510,
-#   season: "fall",
-#   sell_price: 2250
-# )
+# Create Products
+Product.create(livestock_id: bessie.id, farmer_id: farmer.id)
+Product.create(livestock_id: wooly.id, farmer_id: farmer.id)
+
+## Fall Crops
+CropType.create(
+  crop_name: "carrot",
+  days_to_grow: 6,
+  buy_price: 390,
+  season: "fall",
+  sell_price: 1710
+)
+CropType.create(
+  crop_name: "sweet potato",
+  days_to_grow: 6,
+  buy_price: 240,
+  season: "fall",
+  sell_price: 990
+)
+CropType.create(
+  crop_name: "spinach",
+  days_to_grow: 5,
+  buy_price: 440,
+  season: "fall",
+  sell_price: 1890
+)
+CropType.create(
+  crop_name: "eggplant",
+  days_to_grow: 11,
+  buy_price: 480,
+  season: "fall",
+  sell_price: 4140
+)
+CropType.create(
+  crop_name: "bell pepper",
+  days_to_grow: 11,
+  buy_price: 510,
+  season: "fall",
+  sell_price: 2250
+)
